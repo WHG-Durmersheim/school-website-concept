c5df2eba63da3c461bc205cfe5b7a6d947278f60
Domi04151309 <domi04151309business@gmail.com>
2020-06-25 19:21:50 +0200

    Add search page

---
+++ js/search.js
@@ -0,0 +1,22 @@
+// Declare variables
+const query = getUrlParameter('q');
+const filter = query.toUpperCase();
+const list = document.getElementById('posts');
+const listItems = list.getElementsByTagName('div');
+
+// Set results-for
+document.getElementById('results-for').innerHTML = query;
+
+// Loop through all list items, and hide those who don't match the search query
+for (var i = 0; i < listItems.length; i++) {
+  if (listItems[i].querySelector("h1").innerHTML.toUpperCase().indexOf(filter) < 0) {
+    listItems[i].style.display = 'none';
+  }
+}
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
